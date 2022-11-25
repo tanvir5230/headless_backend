58f1b7c3f1a285bfcba2ce181a39ba24fec0d136
tanvir5230 <tanvirbintouhid5230@gmail.com>
2022-11-25 20:24:21 +0600

    cors added for delete operation

--- src_ts/handlers/handlers.ts
+++ src_ts/handlers/handlers.ts
@@ -8,7 +8,7 @@ import {
 
 const headers = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+  "Access-Control-Allow-Methods": "OPTIONS, POST, GET, DELETE",
   "Access-Control-Max-Age": 2592000, // 30 days
   /** add other headers as per requirement */
 };
